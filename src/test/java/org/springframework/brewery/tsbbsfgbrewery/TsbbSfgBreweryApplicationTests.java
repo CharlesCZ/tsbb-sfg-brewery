05ba5527f4f416dd37845fb4fa58089556173dfa
czarek <karakankaczyn@gmail.com>
2019-09-09 16:01:49 +0200

    Initial commit

---
+++ src/test/java/org/springframework/brewery/tsbbsfgbrewery/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,16 @@
+package org.springframework.brewery.tsbbsfgbrewery;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
